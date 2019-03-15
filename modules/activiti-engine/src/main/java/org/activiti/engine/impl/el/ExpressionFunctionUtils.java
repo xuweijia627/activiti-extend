5997d9287fbe5599330e3ac2fd1d5bf5283d8328
xuweijia <xuweijiajia@126.com>
2019-03-15 18:16:55 +0800

    ss

--- modules/activiti-engine/src/main/java/org/activiti/engine/impl/el/ExpressionFunctionUtils.java
+++ modules/activiti-engine/src/main/java/org/activiti/engine/impl/el/ExpressionFunctionUtils.java
@@ -7,7 +7,12 @@ import org.apache.commons.lang3.StringUtils;
  * @date 2018/12/18
  */
 public class ExpressionFunctionUtils {
-
+	public static final String GT = ">";
+    public static final String LT = "<";
+    public static final String EQ = "==";
+    public static final String GE = ">=";
+    public static final String LE = "<=";
+    public static final String NE = "!=";
 	/**
 	 * 在指定比较符时 time与 当前时间相比较
 	 * @param time
@@ -16,15 +21,21 @@ public class ExpressionFunctionUtils {
 	 */
     public static boolean now(long time,CharSequence operator){
     	boolean flag=false;
-    	if(StringUtils.isNotBlank(operator)) {
+    	if(StringUtils.isNotBlank(operator) && time != 0) {
     		long now=System.currentTimeMillis();
-    		if(operator.equals(">")) {
-    			flag=time>now;
-    		}else if(operator.equals("<")) {
-    			flag=time<now;
-    		}else if(operator.equals("==")) {
-    			flag=time==now;
-    		}
+    		if (operator.equals(GT)) {
+                flag = time > now;
+            } else if (operator.equals(LT)) {
+                flag = time < now;
+            } else if (operator.equals(EQ)) {
+                flag = time == now;
+            } else if (operator.equals(GE)) {
+                flag = time >= now;
+            } else if (operator.equals(LE)) {
+                flag = time <= now;
+            } else if (operator.equals(NE)) {
+                flag = time != now;
+            }
     	}
         return flag;
     }
